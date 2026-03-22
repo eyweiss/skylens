192b3d2fdccec827499c6c34d2c4e7aa7576c0b6
Eweiss1 <eyalw82@gmail.com>
2026-03-22 13:41:46 +0200

    update news query

--- app/api/news/route.ts
+++ app/api/news/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 const NEWSAPI_QUERY =
-  "commercial aviation OR airline OR airlines OR airport OR Boeing OR Airbus OR FAA OR EASA";
+  "commercial aviation OR airline OR airlines OR airport OR Boeing OR Airbus OR FAA OR EASA OR American Airlines OR Delta AirLines OR United Airlines OR Southwest Airlines OR Air Canada OR Lufthansa OR Ryanair OR Air France-KLM OR IAG / British Airways OR easyJet OR Emirates OR Qatar Airways OR Etihad Airways OR Turkish Airlines OR flydubai OR Singapore Airlines OR Cathay Pacific OR All Nippon Airways OR Japan Airlines OR Qantas OR airport OR airports OR Hartsfield-Jackson Atlanta OR Los Angeles International OR Dallas OR Fort Worth OR Denver International OR Chicago O'Hare OR London Heathrow OR Paris Charles de Gaulle OR Amsterdam Schiphol OR Frankfurt Airport OR Madrid Barajas OR Dubai International OR Istanbul Airport OR Doha Hamad International OR Riyadh King Khalid OR Abu Dhabi International OR Tokyo Haneda OR Beijing Capital OR Singapore Changi OR Sydney Kingsford Smith OR Hong Kong International)";
 
 export async function GET() {
   const apiKey = process.env.NEWSAPI_KEY;
