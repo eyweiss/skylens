911418cf77cffd4f9a3fdb3ac082b4682c5cd6c5
Eweiss1 <eyalw82@gmail.com>
2026-03-23 15:43:32 +0200

    add google analytics tracking to layout.tsx using gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 const geist = Geist({
   variable: "--font-geist-sans",
@@ -23,6 +24,19 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${geist.variable} h-full antialiased`} suppressHydrationWarning>
       <body className="min-h-full flex flex-col">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-M4M3S1EYKN"
+          strategy="afterInteractive"
+          async
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-M4M3S1EYKN');
+          `}
+        </Script>
         <ThemeProvider>
           {children}
           <Analytics />
